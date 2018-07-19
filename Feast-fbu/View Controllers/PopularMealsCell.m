caaac063d51e166a71fb3ecc2456cd5ce9209d62
Trevon Wiggs <tre@fb.com>
2018-07-19 10:08:22 -0700

    started top meals cell

---
+++ Feast-fbu/View Controllers/PopularMealsCell.m
@@ -0,0 +1,24 @@
+//
+//  PopularMealsCell.m
+//  Feast-fbu
+//
+//  Created by Trevon Wiggs on 7/19/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import "PopularMealsCell.h"
+
+@implementation PopularMealsCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
