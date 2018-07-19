fbbdd341928e7dbfda10a0d64c5dd783488adce9
Jessica Au <jessicaau@fb.com>
2018-07-19 10:23:38 -0700

    testing views

---
+++ RecipeTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.m
+//  Feast-fbu
+//
+//  Created by Jessica Au on 7/19/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import "RecipeTableViewCell.h"
+
+@implementation RecipeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
