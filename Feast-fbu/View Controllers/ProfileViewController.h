b8523519da6e0e2c2bad4fd827add87d3b3e3eb9
Jessica Shu <jessicashu7@fb.com>
2018-07-19 10:08:36 -0700

    basics for profile done

---
+++ Feast-fbu/View Controllers/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Feast-fbu
+//
+//  Created by Jessica Shu on 7/19/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
