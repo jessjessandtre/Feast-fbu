a871d33d3e4ec4fb514f8c35310cd085a1b346a9
Jessica Au <jessicaau@fb.com>
2018-07-23 15:02:19 -0700

    setting up profile view controller for users outside of the current user

---
+++ Feast-fbu/Views/ExternalUserCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ExternalUserCollectionViewCell.h
+//  Feast-fbu
+//
+//  Created by Jessica Au on 7/23/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Post.h"
+
+@interface ExternalUserCollectionViewCell : UICollectionViewCell
+
+@property (strong, nonatomic) Post *post; 
+
+@end
