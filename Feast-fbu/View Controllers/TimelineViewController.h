cf24b6f309e3434ad8493b0a93923b43c6876e8b
Trevon Wiggs <tre@fb.com>
2018-07-23 15:29:00 -0700

    timeline view

---
+++ Feast-fbu/View Controllers/TimelineViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimelineViewController.h
+//  Feast-fbu
+//
+//  Created by Trevon Wiggs on 7/23/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineViewController : UIViewController
+
+@end
---
+++ Feast-fbu/Views/TimelineViewCell.m
@@ -0,0 +1,63 @@
+//
+//  TimelineViewCell.m
+//  Feast-fbu
+//
+//  Created by Trevon Wiggs on 7/23/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import "TimelineViewCell.h"
+#import "DateTools.h"
+
+@implementation TimelineViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)setPost:(Post *)post {
+    _post = post;
+    
+    self.postImage.file = post[@"image"];
+    [self.postImage loadInBackground];
+    
+    
+    // Creates a circle frame for the profile pic
+    self.profilePicture.layer.cornerRadius = self.profilePicture.frame.size.width / 2;
+    
+    self.postAuthorHeader.text = post.user.username;
+    self.postAuthorCaption.text = post.user.username;
+    self.recipeName.text = post.recipe;
+    self.postCaption.text = post.caption;
+    
+    
+    // Format createdAt date string
+    NSDate *createdAtOriginalString = self.post.createdAt;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    // Configure the input format to parse the date string
+    formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+    
+    //Convert String to timeAgo
+    NSString *timeAgoDate = [NSDate shortTimeAgoSinceDate:createdAtOriginalString];
+    self.createdAt = timeAgoDate;
+    self.postTimestamp.text = self.createdAt;
+    
+    
+}
+
+- (IBAction)didTapLike:(id)sender {
+}
+
+- (IBAction)didTapComment:(id)sender {
+}
+
+- (IBAction)didTapShare:(id)sender {
+}
+@end
