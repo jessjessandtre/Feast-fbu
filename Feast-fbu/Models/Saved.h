6ca4659ae12ed5684254a0a80fa109df43fbdb40
Jessica Shu <jessicashu7@fb.com>
2018-07-23 18:04:22 -0700

    created "saved" recipe to user model

---
+++ Feast-fbu/Models/Saved.h
@@ -0,0 +1,18 @@
+//
+//  Saved.h
+//  Feast-fbu
+//
+//  Created by Jessica Shu on 7/23/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+#import "Recipe.h"
+
+@interface Saved : PFObject
+
+@property (strong, nonatomic) PFUser* user;
+@property (strong, nonatomic) Recipe* savedRecipe;
+
+@end
---
+++ Feast-fbu/Models/Saved.m
@@ -0,0 +1,17 @@
+//
+//  Saved.m
+//  Feast-fbu
+//
+//  Created by Jessica Shu on 7/23/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import "Saved.h"
+
+@implementation Saved
+
++ (nonnull NSString *)parseClassName {
+    return @"Saved";
+}
+
+@end
