0363ef752137347398e1feca9e890a20f7c2db89
Jessica Shu <jessicashu7@fb.com>
2018-07-25 11:53:01 -0700

    added collectionviewcell and view controller files for saved recipes

---
+++ Feast-fbu/View Controllers/SavedRecipesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SavedRecipesViewController.h
+//  Feast-fbu
+//
+//  Created by Jessica Shu on 7/25/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedRecipesViewController : UIViewController
+
+@end
---
+++ Feast-fbu/View Controllers/SavedRecipesViewController.m
@@ -0,0 +1,37 @@
+//
+//  SavedRecipesViewController.m
+//  Feast-fbu
+//
+//  Created by Jessica Shu on 7/25/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import "SavedRecipesViewController.h"
+
+@interface SavedRecipesViewController ()
+
+@end
+
+@implementation SavedRecipesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
