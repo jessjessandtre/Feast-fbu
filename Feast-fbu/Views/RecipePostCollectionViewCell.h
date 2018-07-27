285c2dee82b373583111a20fbd2301349e990a23
Jessica Au <jessicaau@fb.com>
2018-07-26 17:34:12 -0700

    adding posts to detail view controller

---
+++ Feast-fbu/Views/RecipePostCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RecipePostCollectionViewCell.h
+//  Feast-fbu
+//
+//  Created by Jessica Au on 7/26/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipePostCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Feast-fbu/Views/RecipePostCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  RecipePostCollectionViewCell.m
+//  Feast-fbu
+//
+//  Created by Jessica Au on 7/26/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import "RecipePostCollectionViewCell.h"
+
+@implementation RecipePostCollectionViewCell
+
+@end
