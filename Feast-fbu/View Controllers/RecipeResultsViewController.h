564b8f34a711882475c4e3cea7c29393ea15f77b
Jessica Shu <jessicashu7@fb.com>
2018-08-03 16:59:44 -0700

    added recipe results, moved fonts

---
+++ Feast-fbu/View Controllers/RecipeResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeResultsViewController.h
+//  Feast-fbu
+//
+//  Created by Jessica Shu on 8/3/18.
+//  Copyright © 2018 jessjessandtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeResultsViewController : UIViewController
+
+@end
