518b18c6b68c38a441727f874c72073d50a70989
Jessica Shu <jessicashu7@fb.com>
2018-08-04 21:53:43 -0700

    segue from recipe results to detaild recipe

--- Feast-fbu/View Controllers/RecipeResultsViewController.m
+++ Feast-fbu/View Controllers/RecipeResultsViewController.m
@@ -10,6 +10,7 @@
 #import "RecipeResultsTableViewCell.h"
 #import <Parse/Parse.h>
 #import "SVProgressHUD.h"
+#import "RecipeDetailViewController.h"
 
 @interface RecipeResultsViewController () <UITableViewDelegate, UITableViewDataSource>
 
@@ -55,14 +56,19 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.recipes.count;
 }
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"DetailedRecipeSegue4"]){
+        RecipeDetailViewController* recipeDetail = [segue destinationViewController];
+        RecipeResultsTableViewCell* cell = (RecipeResultsTableViewCell*)sender;
+        recipeDetail.recipe = cell.recipe;
+    }
 }
-*/
+
 
 @end
