92b8db45f8829196bfb6a53968a9fe39ce1bc9dd
Trevon Wiggs <tre@fb.com>
2018-08-06 09:33:05 -0700

    profile image ui changes

--- Feast-fbu/View Controllers/DetailedPostViewController.m
+++ Feast-fbu/View Controllers/DetailedPostViewController.m
@@ -93,6 +93,11 @@
     }
     [self.profileImageView loadInBackground];
     
+    self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2;
+    self.profileImageView.clipsToBounds = true;
+    self.profileImageView.layer.borderColor = [UIColor colorWithRed:0.96 green:0.00 blue:0.00 alpha:1.0].CGColor;
+    self.profileImageView.layer.borderWidth = 1.5;
+    
     
     self.postImageView.file = self.post.image;
     [self.postImageView loadInBackground];
