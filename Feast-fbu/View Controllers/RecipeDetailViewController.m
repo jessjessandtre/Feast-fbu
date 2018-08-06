69b8939718fd39fd5539fe368c7fb139b4ecc11c
Jessica Au <jessicaau@fb.com>
2018-08-06 08:06:25 -0700

    Compress quality of images posted for faster load time

--- Feast-fbu/View Controllers/RecipeDetailViewController.m
+++ Feast-fbu/View Controllers/RecipeDetailViewController.m
@@ -128,15 +128,31 @@
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(nonnull NSDictionary<NSString *,id> *)info {
     // Get thew image captured by the UIImagePickerController
-    //  UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+
     UIImage *editedImage = info[UIImagePickerControllerEditedImage];
     
     // Do something with the images (based on your use case)
+    CGSize imageSize = CGSizeMake(600, 600);
+    editedImage = [self resizeImage:editedImage withSize:imageSize];
     
     // Dismess UIImagePickerController to go back to your original view controller
     [self dismissViewControllerAnimated:YES completion:^(){
         [self performSegueWithIdentifier:@"CreatePostSegue" sender:editedImage];
     }];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
