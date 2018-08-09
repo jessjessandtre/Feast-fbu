f158403523b26db2d2a57af285140b0038342e06
Jessica Shu <jessicashu7@fb.com>
2018-08-09 16:04:30 -0700

    fixing merge conflict

--- Feast-fbu/View Controllers/DiscoveryViewController.m
+++ Feast-fbu/View Controllers/DiscoveryViewController.m
@@ -304,35 +304,35 @@
         
         [save setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [save setTitleColor:[UIColor blackColor] forState:UIControlStateSelected];
-
+        
         UIImage *saveImage = [UIImage imageNamed:@"bookmark-outline.png"];
         [save setImage:saveImage forState:UIControlStateNormal];
         UIImage *saveImage2 = [UIImage imageNamed:@"bookmark.png"];
         [save setImage:saveImage2 forState:UIControlStateSelected];
-
+        
         [save setBackgroundColor:[UIColor colorWithRed:0.97 green:0.97 blue:0.97 alpha:1.0]];
-
+        
         UIButton* share = [[UIButton alloc] initWithFrame:CGRectMake(0, -50, 100, 100)];
 //        [share setTitle:@"share" forState:UIControlStateNormal];
 //        [share setTitle:@"unshare" forState:UIControlStateSelected];
         [share setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [share setTitleColor:[UIColor blackColor] forState:UIControlStateSelected];
         [share addTarget:self action:@selector(onShareTapped:) forControlEvents:UIControlEventTouchUpInside];
-
+        
         UIImage *shareImage = [UIImage imageNamed:@"share.png"];
         [share setImage:shareImage forState:UIControlStateNormal];
         [share setBackgroundColor:[UIColor colorWithRed:0.97 green:0.97 blue:0.97 alpha:1.0]];
-
+        
         UIStackView* stackView = [[UIStackView alloc] initWithArrangedSubviews:@[save,share] ];
         stackView.frame = CGRectMake(0, 0, 80, 80);
         stackView.axis = UILayoutConstraintAxisVertical;
         stackView.distribution = UIStackViewDistributionFillEqually;
         save.layer.zPosition = 1;
-
-
+        
+        
         cell.leftButtons = @[stackView];
         cell.leftSwipeSettings.transition = MGSwipeTransitionStatic;
-
+        
         arr = @[stackView];
 
     }
